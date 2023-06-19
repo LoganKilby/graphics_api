9b934131073a98591cd4d7ab70ac472f9442e759
Logan Kilby <logankilby@gmail.com>
2023-06-19 09:35:39 -0500

    fixed camera panning

--- camera.cpp
+++ camera.cpp
@@ -39,10 +39,8 @@ void pan_orbit_camera(Orbit_Camera *camera, v2 mouse_diff) {
         assert(camera->pan_speed > 0);
         camera->target += camera->basis.right * -mouse_diff.x * camera->pan_speed * Platform.delta_time;
         
-        // TODO(lmk): when the camera is oriented down the y axis, we cannot pan around the xz plane because of the way
-        // we're multiplying the pan vector..
-        v3 pan_vector = project_onto_plane(camera->basis.front, UP);
-        camera->target +=  pan_vector * -mouse_diff.y * camera->pan_speed * Platform.delta_time;
+        v3 pan_vector = normalize(cross(camera->basis.right, UP));
+        camera->target +=  pan_vector * mouse_diff.y * camera->pan_speed * Platform.delta_time;
     }
 }
 
